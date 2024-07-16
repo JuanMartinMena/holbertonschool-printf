24f1d9de0d4a9da7944ec6ac7c758ecd859cafac
JuanMartinMena <juanmartinmenac@gmail.com>
2024-07-16 23:07:56 +0000

    upgrate the logic of the code

--- main.c
+++ main.c
@@ -7,12 +7,6 @@
  */
 int main(void)
 {
-	int len = 0;
-
-	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-	_printf("%%--------------------------------------------\n\n");
-	printf("%\n");
 	_printf("%");
-	len = _printf("Let's try to printf a simple sentence.\n");
-	return (len);
+	return (0);
 }
